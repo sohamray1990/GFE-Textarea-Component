ed20e5dbfbecfb1af085835b960619bcfa2c844f
Soham Ray <sohamray@Sohams-Laptop.local>
2025-01-25 17:18:45 +0530

    added code to restrict input value beyond max limit

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
 const descriptionField1 = document.querySelector('#description-field1');
+const maxChars = 500;
 
 descriptionField1.addEventListener('input', (event) => {
   const count = event.target.value.length;
   const countElement = document.querySelector('#description-field-count1');
-  countElement.textContent = event.target.value.length;
-
   const countParentElement = countElement.parentElement;
-  if (count > 500) {
+
+    // If the user enters more than 500 characters, show error and truncate the input value
+  if (count > maxChars) {
     countParentElement.classList.add('error');
     descriptionField1.classList.add('error');
+    event.target.value = event.target.value.substring(0, maxChars);
   } else {
     countParentElement.classList.remove('error');
     descriptionField1.classList.remove('error');
   }
+
+  // Update the character count after handling truncation
+  countElement.textContent = event.target.value.length;
 });
